1169d0cbbfeb12c49f672b91676288454f35bcf2
Aron Tolentino <arontolentino@outlook.com>
2020-02-08 16:55:59 -0500

    Create basic ability to create cards under lists and make it sortable

--- scripts/script.js
+++ scripts/script.js
@@ -2,15 +2,29 @@ $(document).ready(function() {
 	// Make lists sortable
 	$('.board').sortable();
 
+	// Make cards sortable
+	$('.list').sortable();
+
 	// Event listener for creating new lists
-	$('input[name="newList"]').keyup(function(event) {
+	$('#newList').keyup(function(event) {
 		if (event.key == 'Enter' || event.keyCode == '13') {
 			$(this).before(
 				'<div class="list"><h2 class="listHeader">' +
 					$(this).val() +
-					'</h2><input type="text" class="newCard" name="newCard" placeholder="New Card..." /></div>'
+					'</h2><input type="text" class="newCard" id="newCard" name="newCard" placeholder="New Card..." /></div>'
 			);
 			$(this).val('');
 		}
+	});
+
+	// Event listener for creating new cards
+	$('.board').on('keyup', '#newCard', function(e) {
+		if (event.key === 'Enter' || event.keyCode === '13') {
+			console.log('New card');
+			$(this).before('<div class="card">' + $(this).val() + '</div>');
+			$(this).val('');
+		}
+
+		$('.list').sortable();
 	});
 });
