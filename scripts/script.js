65cef7385135ad2f010616619a42b9893f6940e3
Aron Tolentino <arontolentino@outlook.com>
2020-02-10 12:25:53 -0500

    Fix firestore first argument error

--- scripts/script.js
+++ scripts/script.js
@@ -127,19 +127,6 @@ function createSortableCards() {
 	});
 }
 
-db.collection('lists')
-	.doc(this.id)
-	.get()
-	.then(function(querySnapshot) {
-		querySnapshot.forEach(function(doc) {
-			// doc.data() is never undefined for query doc snapshots
-			console.log(doc.id, ' => ', doc.data());
-		});
-	})
-	.catch(function(error) {
-		console.log('Error getting documents: ', error);
-	});
-
 // MAIN
 
 $(document).ready(function() {
